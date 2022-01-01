e2cfbbf00a5a3884f96d7809f2608f3236b09874
mazharul-islam321 <https://github.com/mazharul-islam321/School-Management-System.git>
2022-01-02 01:15:02 +0600

    new cod

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h3>Hello from Mizba Uddin Tareq</h3>
-      <h2>Monira asteche</h2>
+    return (
+        <div className="App">
+            <h3>Hello from Mizba Uddin Tareq</h3>
+            <h2>Monira asteche</h2>
 
-      <h3>asif mohammed</h3>
+            <h3>asif mohammed</h3>
 
-      <h2>moni changes</h2>
-      <p>new change</p>
-      <h3>Hello its me from test mizba</h3>
+            <h2>moni changes</h2>
+            <p>new change</p>
+            <h3>Hello its me from test mizba</h3>
 
-      <h2>moni changes</h2>
-      <p>new change</p>
-      <h3>Hello its me from test mizba</h3>
+            <h2>moni changes</h2>
+            <p>new change</p>
+            <h3>Hello its me from test mizba</h3>
 
-      <h6>Hi from arvind</h6>
-      <h6>Hi from asif mohammed</h6>
-<<<<<<< HEAD
-      <h6>Hi again from arvind</h6>
-      <h2>from moni</h2>
-      <h3>Changes again from mizba uddin tareq</h3>
-=======
-      <h6>Hi from manum vai</h6>
->>>>>>> asif
-    </div>
-  );
+            <h6>Hi from arvind</h6>
+            <h6>Hi from asif mohammed</h6>
+            <h6>Hi again from arvind</h6>
+            <h2>from moni</h2>
+            <h3>Changes again from mizba uddin tareq</h3>
+            <h6>Hi from manum vai</h6>
+
+            <p>git branch mamun,, git checkout mamun,, 3 main code </p>
+            <p>git branch mamun,, git checkout mamun,, 3 main code </p>
+        </div>
+    );
 }
 
 export default App;
