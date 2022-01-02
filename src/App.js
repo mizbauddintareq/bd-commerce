d69c331baf1225df1d73ff642561306a59f56a16
XAWAD MUSA <moniinfo60@gmail.com>
2022-01-02 15:57:16 +0600

    folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./Components/Shared/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h3>Hello from Mizba Uddin Tareq</h3>
-      <h2>Monira asteche</h2>
-      <h2>moni changes</h2>
-      <p>new change</p>
-      <h2>from moni</h2>
-      <p>git branch setup done</p>
-
+      <Footer></Footer>       
     </div>
   );
 }
