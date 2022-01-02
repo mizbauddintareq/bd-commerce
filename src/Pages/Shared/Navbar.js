d753d180ac0cb4adf8650209b47f151455a35d99
Arvind <arbindudebnath59985@gmail.com>
2022-01-02 13:21:51 +0600

    header

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import logo from './logo.png'
-
+import 'animate.css';
 
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='container d-flex justify-content-around align-items-center mt-4'>
             <div>
-                <img src={logo} alt="" />
+                <img className='img-fluid w-50 ' src={logo} alt="" />
             </div>
-            <div>
-                <input type="search" name="" id="" />
-                <select name="" id=""></select>
-                <input type="submit" value="" />
+            <div className='search'>
+                <input style={{backgroundColor:'rgb(239, 239, 239)', marginRight:'1px', width:'300px'}} className='rounded-end rounded-pill border border-1 p-2 ps-3' placeholder='Search...' type="search" name="" id="" />
+                <p id='categories' style={{backgroundColor:'rgb(239, 239, 239)', display:'inline', padding:'9.5px'}} className='border border-1'>All Categories <i class="fas fa-angle-down"></i></p>
+                <input type="submit" className=' px-4 py-2 rounded-start rounded-pill border border-1 ' value="🔎︎" />
             </div>
-            <div>
-                <i class="fas fa-phone-alt"></i>
+            <div className='d-flex'>
+                <h1 className=' me-2 text-primary inline animate__animated animate__shakeX animate__repeat-2'>🕻</h1>
                 <div>
-                    <h6>CALL US NOW</h6>
-                    <h5>+123 4567 890</h5>
+                    <h5 style={{ fontSize: '14px' }} className='text-secondary '> CALL US NOW</h5>
+                    <h5 style={{ fontSize: '18px' }} className=''>+123 4567 890</h5>
                 </div>
             </div>
             <div>
-                <i class="far fa-user"></i>
-                <i class="far fa-heart"></i>
-                <i class="fas fa-shopping-bag"><i class="fas fa-angle-down"></i></i>
+                <i className="far fa-user m-3"></i>
+                <i className="far fa-heart m-3"></i>
+                <i className="fas fa-shopping-bag m-3"><i class="fas fa-angle-down m-1"></i></i>
             </div>
         </div>
     );
