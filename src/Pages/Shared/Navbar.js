54086afebad713ba7e202cae0655886af20e7f7d
Arvind <arbindudebnath59985@gmail.com>
2022-01-02 14:29:56 +0600

    added hover and animation

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.png'
 import 'animate.css';
+import './CSS/Navbar.css'
 
 
 const Navbar = () => {
     return (
         <div className='container d-flex justify-content-around align-items-center mt-4'>
             <div>
-                <img className='img-fluid w-50 ' src={logo} alt="" />
+                <img className='img-fluid w-50' src={logo} alt="" />
             </div>
             <div className='search'>
                 <input style={{backgroundColor:'rgb(239, 239, 239)', marginRight:'1px', width:'300px'}} className='rounded-end rounded-pill border border-1 p-2 ps-3' placeholder='Search...' type="search" name="" id="" />
@@ -15,16 +16,16 @@ const Navbar = () => {
                 <input type="submit" className=' px-4 py-2 rounded-start rounded-pill border border-1 ' value="🔎︎" />
             </div>
             <div className='d-flex'>
-                <h1 className=' me-2 text-primary inline animate__animated animate__shakeX animate__repeat-2'>🕻</h1>
+                <h1 className=' me-2 text-primary inline animate__animated animate__shakeX animate__repeat-3 phone '>🕻</h1>
                 <div>
                     <h5 style={{ fontSize: '14px' }} className='text-secondary '> CALL US NOW</h5>
                     <h5 style={{ fontSize: '18px' }} className=''>+123 4567 890</h5>
                 </div>
             </div>
             <div>
-                <i className="far fa-user m-3"></i>
-                <i className="far fa-heart m-3"></i>
-                <i className="fas fa-shopping-bag m-3"><i class="fas fa-angle-down m-1"></i></i>
+                <i className="far fa-user m-3 fs-5 p-2 user-icon rounded-circle"></i>
+                <i className="far fa-heart m-3 fs-5 p-2 rounded-circle favorite"></i>
+                <i className="fas fa-shopping-bag fs-5 m-3 cart"><span className='rounded-circle fs-6 cart-number'>1</span><i class="fas fa-angle-down ms-2"></i></i>
             </div>
         </div>
     );
