1971ff7fccfb5359238186d95ffdc18cc9e08d25
Arvind <arbindudebnath59985@gmail.com>
2022-01-02 21:59:23 +0600

    something

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ function App() {
   return (
     <div className="App">    
       <HomeMain></HomeMain>
-      <h1>hey hey hey</h1>
-      <h1>hello from asif1</h1>
-      <h1>hello from moni</h1>
-      <h1>hello from mohammed</h1>
        <Footer></Footer>
       
     </div>
