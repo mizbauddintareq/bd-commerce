41d7187823df1b2585e2633d840b61d3237bc852
AsifMohammedSifat <asifmohammedsifat38@gmail.com>
2022-01-02 16:07:13 +0600

    hello

--- src/App.js
+++ src/App.js
@@ -3,59 +3,12 @@ import "./App.css";
 import HomeMain from "./Pages/Home/HomeMain/HomeMain";
 
 function App() {
-<<<<<<< HEAD
-    return (
-        <div className="App">
-            <h3>Hello from Mizba Uddin Tareq</h3>
-            <h2>Monira asteche</h2>
 
-            <h3>asif mohammed</h3>
-
-            <h2>moni changes</h2>
-            <p>new change</p>
-            <h3>Hello its me from test mizba</h3>
-
-            <h2>moni changes</h2>
-            <p>new change</p>
-            <h3>Hello its me from test mizba</h3>
-
-            <h6>Hi from arvind</h6>
-            <h6>Hi from asif mohammed</h6>
-            <h6>Hi again from arvind</h6>
-            <h2>from moni</h2>
-            <h3>Changes again from mizba uddin tareq</h3>
-            <h6>Hi from manum vai</h6>
-
-            <p>git branch mamun,, git checkout mamun,, 3 main code </p>
-            <p>git branch mamun,, git checkout mamun,, 3 main code </p>
-            <p>pmizba vai</p>
-        </div>
-    );
-<<<<<<< HEAD
-=======
   return (
     <div className="App">
       <HomeMain></HomeMain>
     </div>
   );
->>>>>>> arvind
-=======
-  return (
-    <div className="App">
-      <h3>Hello from Mizba Uddin Tareq</h3>
-<<<<<<< HEAD
-      <h3>asif mohammed</h3>
-=======
-      <h2>Monira asteche</h2>
-      <h2>moni changes</h2>
-      <p>new change</p>
-      <h2>from moni</h2>
-      <p>git branch setup done</p>
-
->>>>>>> 8cbb58866c96b62ca636137b2c9d60850790bbba
-    </div>
-  );
->>>>>>> dd5d41c96b8ab2d800f4d47d45e419bc439358bc
 }
 
 export default App;
