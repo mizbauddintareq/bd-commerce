e92cf8563a13d13935676ba83fd085146ce2dbda
Arvind <arbindudebnath59985@gmail.com>
2022-01-02 23:46:01 +0600

    added react router, added about page and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import CustomerReview from "./Pages/About/CustomerReview/CustomerReview";
+import AboutMain from "./Pages/About/AboutMain/AboutMain";
 import HomeMain from "./Pages/Home/HomeMain/HomeMain";
-import Footer from "./Pages/Shared/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <HomeMain></HomeMain>
-       <Footer></Footer>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomeMain/>} />
+        <Route path='/about' element={<AboutMain/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
