4bb33cdd3da025511d82c1f75ff98352dbe10a6a
mizbauddintareq <mizbauddintareq@gmail.com>
2022-01-02 23:26:05 +0600

    changes by mizba

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h3>Hello from Mizba Uddin Tareq</h3>
-      <h2>Monira asteche</h2>
-      <h2>moni changes</h2>
-      <p>new change</p>
-      <h3>Hello its me from test mizba</h3>
-      <h3>Changes again from mizba uddin tareq</h3>
+      
     </div>
   );
 }
